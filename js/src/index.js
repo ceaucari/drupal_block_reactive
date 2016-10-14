f443aeaeef5cb0ed7325fcb2a4ad8639452f21ba
Heshan Wanigasooriya <heshan@heidisoft.com>
2016-10-14 19:31:30 -0400

    Update index.js

--- js/src/index.js
+++ js/src/index.js
@@ -7,16 +7,6 @@ import CommentBox from './components/CommentBox'
 
 Drupal.behaviors.drupal_block_reactive = {
   attach: (context) => {
-    // Comment component.
-    class Comment extends React.Component {
-      render() {
-        return (
-          <div className="Comment">
-            <span>{this.props.subject}</span> | <span>{this.props.changed}</span>
-          </div>
-        );
-      }
-    }
     // Render our component.
     ReactDOM.render(
       <CommentBox url='/api/comments' timeInterval={2000}/>,
