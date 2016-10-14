43fa201fdd6560fa7113cc7e6127dcb4efd6c127
Heshan Wanigasooriya <heshan@heidisoft.com>
2016-10-14 19:31:57 -0400

    Create Comment.js

---
+++ js/src/components/Comment.js
@@ -0,0 +1,6 @@
+// Comment component.
+class Comment extends React.Component {
+  render() {
+    return ( <div className = "Comment" ><span> {this.props.subject} < /span> | <span>{this.props.changed}</span ></div>);
+  }
+}
